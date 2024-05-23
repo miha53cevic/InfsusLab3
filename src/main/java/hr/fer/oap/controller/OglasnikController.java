05bb33825611f76e0871044bfbe7e07e829bf605
Tin Margetić <tin.margetic@fer.hr>
2024-05-23 18:05:25 +0200

    Radi search naziv i kategorija

--- src/main/java/hr/fer/oap/controller/OglasnikController.java
+++ src/main/java/hr/fer/oap/controller/OglasnikController.java
@@ -1,19 +1,55 @@
 package hr.fer.oap.controller;
 
+import hr.fer.oap.domain.Kategorija;
+import hr.fer.oap.mapping.MappingToFilteredOglasi;
+import hr.fer.oap.service.*;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller()
 @RequestMapping("")
 public class OglasnikController {
+    private final OglasService oglasService;
+    private final KategorijaService kategorijaService;
+
+    public OglasnikController(OglasService oglasService, KategorijaService kategorijaService) {
+        this.oglasService = oglasService;
+        this.kategorijaService = kategorijaService;
+    }
+
     @GetMapping("")
     public String index(@AuthenticationPrincipal UserDetails userDetails, Model model) {
         model.addAttribute("page", "oglasnik");
         model.addAttribute("username", userDetails.getUsername());
+        model.addAttribute("oglasi", oglasService.fetchAll());
+        model.addAttribute("kategorije", kategorijaService.fetchAll());
+        return "oglasnik";
+    }
+
+    @GetMapping("/oglasnik")
+    public String dohvatiOglaseForm(@AuthenticationPrincipal UserDetails userDetails, Model model,
+                                    @RequestParam(name = "searchNaziv", required = false) String naziv,
+                                    @RequestParam(name = "kategorija", required = false) Long searchKategorija,
+                                    @RequestParam(name = "istekli", required = false) String istekli) {
+        var oglasi = oglasService.fetchAll();;
+        if (searchKategorija != null) {
+            Kategorija kat = kategorijaService.findById(searchKategorija).get();
+            oglasi = oglasService.findAllByKategorija(kat);
+        }
+
+        if (naziv != null) {
+            oglasi = MappingToFilteredOglasi.fromOglasiByNaziv(oglasi, naziv);
+        }
+        var kategorije = kategorijaService.fetchAll();
+        model.addAttribute("page", "oglasnik");
+        model.addAttribute("username", userDetails.getUsername());
+        model.addAttribute("oglasi", oglasi);
+        model.addAttribute("kategorije", kategorije);
         return "oglasnik";
     }
 
--- src/main/java/hr/fer/oap/mapping/MappingToOglasDuration.java
+++ src/main/java/hr/fer/oap/mapping/MappingToOglasDuration.java
@@ -3,10 +3,11 @@ package hr.fer.oap.mapping;
 import hr.fer.oap.domain.Oglas;
 
 import java.time.Duration;
-
+import java.time.LocalDateTime;
 public class MappingToOglasDuration {
     public static Long oglasToDuration(Oglas oglas) {
-        var duration = Duration.between(oglas.getPocetnoVrijeme(), oglas.getZavrsnoVrijeme());
-        return duration.toHours();
+        var now = LocalDateTime.now();
+        var duration = Duration.between(now, oglas.getPocetnoVrijeme());
+        return duration.toHours() < 0 ? 0L : duration.toHours();
     }
 }
